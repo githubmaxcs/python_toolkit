8dc6b7342dd1e426ec86708eddc5ea4839a0ac59
Maxwell Hanson <maxwellhanson@Maxwells-MacBook-Pro-M4---2024.local>
2026-07-28 13:46:06 -0500

    updating py file with combinatoricsfor error handling

--- src/probability/combinatorics.py
+++ src/probability/combinatorics.py
@@ -7,13 +7,27 @@ def factorial(n):
     return product
 
 def permutation_without_replacement(n, r):
+    if n < 0 or r < 0:
+        raise ValueError("Inputs must be non-negative.")
+    # if r > n, return 0, since it's not feasible
+    if r > n:
+        return 0
     return factorial(n) // factorial(n - r)
 
 def permutation_with_replacement(n, r):
+    if n < 0 or r < 0:
+        raise ValueError("Inputs must be non-negative.")
     return n**r
 
 def combination_without_replacement(n, r):
+    if n < 0 or r < 0:
+        raise ValueError("Inputs must be non-negative.")
+    # if r > n, return 0, since it's not feasible
+    if r > n:
+        return 0
     return factorial(n) // factorial(r) // factorial(n - r)
 
 def combination_with_replacement(n, r):
+    if n < 0 or r < 0:
+        raise ValueError("Inputs must be non-negative.")
     return factorial(n+r-1) // factorial(r) // factorial(n - 1)
